ed701c00aa9ce4bc1b39483de24570b17d215439
N e n e <makiasagawa@gmail.com>
2020-07-01 10:11:02 -0400

    Stanza data

---
+++ POS/stanza_analysis.py
@@ -0,0 +1,60 @@
+import stanza
+import pandas as pd
+import json
+
+data = pd.read_csv("data/flattened_paragraphs.csv", index_col=None)
+raw_sentences = data["sentence"].to_list()
+# stanza.download('en')
+
+nlp = stanza.Pipeline(lang='en', processors='tokenize,mwt,pos')
+verb_dict = {}
+# for sentence in sentences:
+all_words_dict = {}
+l = len(raw_sentences)
+
+i = 0
+for s in raw_sentences:
+    print(i/l)
+    doc = nlp(s)
+    for sent in doc.sentences:
+        for word in sent.words:
+            # if no word yet, add the frame
+            if word.text not in all_words_dict:
+                all_words_dict[word.text] = {"word_count":1,
+                                            "upos": {},
+                                            "xpos": {},
+                                            "feats": {}}
+            # else update count
+            else:
+                all_words_dict[word.text]["word_count"] += 1 
+
+            # all data to the frame
+            if word.upos not in all_words_dict[word.text]["upos"]:
+                all_words_dict[word.text]["upos"][word.upos] = 1
+            else:
+                all_words_dict[word.text]["upos"][word.upos] += 1 
+
+            if word.xpos not in all_words_dict[word.text]["xpos"]:
+                all_words_dict[word.text]["xpos"][word.xpos] = 1
+            else:
+                all_words_dict[word.text]["xpos"][word.xpos] += 1 
+
+            if word.feats not in all_words_dict[word.text]["feats"]:
+                all_words_dict[word.text]["feats"][word.feats] = 1
+            else:
+                all_words_dict[word.text]["feats"][word.feats] += 1 
+
+
+            if str(word.upos) == "VERB":
+                if word in verb_dict:
+                    verb_dict[word.text] += 1
+                else:
+                    verb_dict[word.text] = 1
+    i+=1
+
+with open('verbs.json', 'w') as fp:
+    json.dump(str(verb_dict), fp, indent=4)
+
+with open('words.json', 'w') as fp:
+    json.dump(str(all_words_dict), fp, indent=4)
+
